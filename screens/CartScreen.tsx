fe396b2d593f58906da80fa340f36e6cee3db65c
minast1 <edmarfo2@gmail.com>
2023-02-23 01:08:46 +0000

    minor fixes

--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -5,12 +5,44 @@ import { useQuery } from "@tanstack/react-query";
 import { dataProps, getUserCart } from "../src/lib/api";
 import CartItem from "../components/CartItem";
 import { FontAwesome } from "@expo/vector-icons";
+import LottieView from "lottie-react-native";
 
 const CartScreen = () => {
   const { isLoading, data } = useQuery<dataProps[] | []>(["cart"], () =>
     getUserCart()
   );
-
+  if (data?.length == 0)
+    return (
+      <View flex={1} bg="white">
+        <VStack w="100%">
+          <Box bg="gray.200" py="4" justifyContent="center" px="2">
+            <Text color="gray.500" fontSize="md">
+              Cart Summary
+            </Text>
+          </Box>
+          <HStack
+            bg="gray.100"
+            py="4"
+            justifyContent="space-between"
+            alignItems="center"
+            px="2"
+          >
+            <Text fontSize="lg">Subtotal</Text>
+            <Text fontSize="lg" fontWeight="bold">
+              {" "}
+              GH₵ 0.00
+            </Text>
+          </HStack>
+        </VStack>
+        <View flex={1} bg="white" alignItems="center" justifyContent="center">
+          <LottieView
+            autoPlay
+            style={{ width: 250, height: 250 }}
+            source={require("../assets/icons/emptyCart.json")}
+          />
+        </View>
+      </View>
+    );
   return (
     <View flex={1} bg="white">
       <VStack w="100%">
